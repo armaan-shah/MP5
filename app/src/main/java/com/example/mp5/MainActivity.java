baf137858726234b182f5682874600f94e2d929c
sarodnori <sarodn2@illinois.edu>
2019-04-19 13:53:14 -0500

    Created a UI that opens Google once the "I'm done" button is clicked.

--- app/src/main/java/com/example/mp5/MainActivity.java
+++ app/src/main/java/com/example/mp5/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.mp5;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.mp5.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent URL = new Intent(android.content.Intent.ACTION_VIEW);
+        URL.setData(Uri.parse("http://www.google.com/"));
+        /*
+        EditText userInput = (EditText) findViewById(R.id.inputBox);
+        String message = userInput.getText().toString();
+        URL.putExtra(EXTRA_MESSAGE, message);
+        */
+        startActivity(URL);
     }
 }
---
+++ app/src/main/java/com/example/mp5/Tasks.java
@@ -0,0 +1,6 @@
+package com.example.mp5;
+
+class Tasks {
+    /** Subscription key. */
+    private static final String SUBSCRIPTION_KEY = BuildConfig.API_KEY;
+}
