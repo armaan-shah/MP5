0d187d63b3056792f8df8e3bb2ba4dec09c0bbc8
sarodnori <sarodn2@illinois.edu>
2019-04-26 10:12:48 -0500

    Started work on API stuff.

--- app/src/main/java/com/example/mp5/MainActivity.java
+++ app/src/main/java/com/example/mp5/MainActivity.java
@@ -7,12 +7,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+
+import com.android.volley.RequestQueue;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MP5:Main";
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Spinner dietSpinner = findViewById(R.id.dietSpinner);
         Spinner cuisineSpinner = findViewById(R.id.cuisineSpinner);
+        Spinner intolerancesSpinner = findViewById(R.id.intolerancesSpinner);
+        Spinner typeSpinner = findViewById(R.id.typeSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.diet_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -28,10 +34,22 @@ public class MainActivity extends AppCompatActivity {
                 R.array.cuisine_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         cuisineSpinner.setAdapter(adapter);
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.intolerances_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        intolerancesSpinner.setAdapter(adapter);
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.type_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeSpinner.setAdapter(adapter);
+        removeProgressBar();
     }
-
+    public void removeProgressBar() {
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+    }
     protected void onClick() {
-
+        new Tasks.ProcessImageTask(MainActivity.this, requestQueue);
     }
     public void sendMessage(View view) {
         Intent URL = new Intent(android.content.Intent.ACTION_VIEW);
